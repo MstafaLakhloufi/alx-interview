416cc82f688de23082d599d4e6c989423a00fe1c
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-10-06 21:23:08 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""
-Defines a function that returns a list of lists
-of integers that represents the pascal's triangle.
-"""
+"""Pascal Triangle Interview Challenge"""
+
 
 def pascal_triangle(n):
-    """
-    Returns a list of lists representing Pascal's triangle up to level n.
-    """
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]  # First row is always [1]
+    triangle = [0] * n
 
-    for i in range(1, n):
-        row = [1]  # Every row starts with 1
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            # Sum of two elements from the previous row
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # Every row ends with 1
-        triangle.append(row)
+            if j > 0 and j < len(new_row):
+                a = triangle[i - 1][j]
+                b = triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        triangle[i] = new_row
 
     return triangle
