03175f69aa59bfbecbc833eb49f5644e7845d389
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-11-23 17:13:07 +0100

    retate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,8 +8,9 @@ def rotate_2d_matrix(matrix) :
     rotates a 2d matrix 90° clockwise
     Returns: Nothing
     '''
-    for i in range(len(matrix)):
-        for j in range(i+1, len(matrix)):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for row in matrix:
