0bbb12ba2236e9cbf071381f80febe20e166c3fc
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-11-28 14:46:35 +0100

    updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 """A function to determine the fewest number of coins needed
-   to meet a given amount total."""
+   to meet a given amount total"""
+
 
 def makeChange(coins, total):
-    """
-    Calculate the minimum number of coins needed to achieve the total.
-
-    Args:
-        coins (list): Available coin denominations.
-        total (int): Target total to achieve.
-
-    Returns:
-        int: Fewest number of coins needed, or -1 if not possible.
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
     """
     if total <= 0:
         return 0
@@ -27,17 +21,3 @@ def makeChange(coins, total):
         if total == 0:
             return counter
         return -1
-    '''if total <= 0:
-        return 0
-
-    coins.sort(reverse=True)  
-    counter = 0
-
-    for coin in coins:
-        if total == 0:
-            break
-        count, total = divmod(total, coin)
-        counter += count
-
-    return counter if total == 0 else -1'''
-    
